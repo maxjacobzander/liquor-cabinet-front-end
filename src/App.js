6b23f29538aec4f1f31d53dc656ea260570d53c1
maxjacobzander <maxjacobzander@gmail.com>
2021-01-14 11:28:51 -0500

    imported createStore, applyMiddleware, thunk, and Provider to index.js

--- src/App.js
+++ src/App.js
@@ -2,11 +2,11 @@ import React, { Component } from 'react';
 
 class App extends Component {
 
-  componentDidMount() {
-    fetch(`http://localhost:3001/api/v1/users/`)
-    .then(resp => resp.json())
-    .then(data => console.log(data))
-  }
+  // componentDidMount() {
+  //   fetch(`http://localhost:3001/api/v1/users/`)
+  //   .then(resp => resp.json())
+  //   .then(data => console.log(data))
+  // }
 
   render(){
     return (
