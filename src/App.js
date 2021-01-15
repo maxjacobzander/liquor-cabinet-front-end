ab2b106cb4819d932afcd02a6d6e84f4f7709c6b
maxjacobzander <maxjacobzander@gmail.com>
2021-01-15 09:39:42 -0500

    created images folder to render photo and initial css stylesheet created

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
 import React, { Component } from 'react';
 import {connect} from 'react-redux';
 import Home from './components/Home';
+import './index.css';
 
 class App extends Component {
 
   componentDidMount() {
-    // fetch(`http://localhost:3001/api/v1/users/`)
-    // .then(resp => resp.json())
-    // .then(data => console.log(data))
-  }
+  //   fetch(`http://localhost:3001/api/v1/liquors/1/drinks`)
+  //   .then(resp => resp.json())
+  //   .then(data => console.log(data))
+  // }
 
   render(){
     return (
       <div className="App">
-        App
         <Home />
       </div>
     );
--- src/components/home.js
+++ src/components/home.js
@@ -1,10 +1,18 @@
 import React, { Component } from 'react';
+import Logo from '../images/logoText.png';
 
 class Home extends Component {
     render() {
         return (
             <div>
-               This is a test.
+                <div>
+                <h2>Welcome to</h2>
+                <img class="logo" src={Logo} alt="The Liquor Cabinet" />
+                </div>
+                <div id="age_verification">
+                    Are You 21 or Older?
+                    <button>Yes</button>
+                </div>
             </div>
         );
     }
