eb34775a194726fb4b502af25e4200053016faf9
maxjacobzander <maxjacobzander@gmail.com>
2021-01-18 14:40:38 -0500

    Updated actions to reflect single model change

--- src/actions/actions.js
+++ src/actions/actions.js
@@ -1,17 +1,18 @@
-export const fetchLiquors = () => {
+export const fetchDrinks = () => {
     return(dispatch) => {
-        return fetch(`http://localhost:3001/api/v1/liquors/{this.id}`)
+        return fetch(`http://localhost:3001/api/v1/drinks`)
         .then(resp => resp.json())
-        .then(liquor => {
-            dispatch({ type: 'SET_LIQUORS', payload: liquors})
-        })
+        .then(data => console.log)
+        // .then(liquor => {
+        //     dispatch({ type: 'SET_LIQUORS', payload: liquors})
+        // })
     }
 }
 
 
 export const addDrink = drink => {
     return(dispatch) => {
-        return fetch('http://localhost:3001/api/v1/liquors/7', {
+        return fetch('http://localhost:3001/api/v1/drinks', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
