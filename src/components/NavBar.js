464291b9674bc619ca4e1c0aa5d949ccdc173c75
maxjacobzander <maxjacobzander@gmail.com>
2021-01-19 15:30:07 -0500

    Added line breaks into NavBar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -5,15 +5,20 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 export class NavBar extends Component {
   render() {
     return (
-      <nav className="nav-bar">
-      <Router>
-        <ul className="nav-container">
-          <NavLink to="/">Home</NavLink>
-            <li><NavLink to="/drinks/new">Add A Drink</NavLink></li>
-            <li><NavLink to="/drinks">View All Drinks</NavLink></li>
-          </ul>
-        </Router>
-      </nav>
+      <div>
+        <br />
+        <br />
+        <br />
+        <nav className="nav-bar">
+        <Router>
+          <ul className="nav-container">
+            <NavLink to="/">Home</NavLink>
+              <li><NavLink to="/drinks/new">Add A Drink</NavLink></li>
+              <li><NavLink to="/drinks">View All Drinks</NavLink></li>
+            </ul>
+          </Router>
+        </nav>
+      </div>
     )
   }
 }
