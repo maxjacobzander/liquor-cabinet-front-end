46898d8ea81d16463f9d8eca0cfe3ceb3e229a44
maxjacobzander <maxjacobzander@gmail.com>
2021-01-19 15:18:43 -0500

    Removed Manhattan from default state

--- src/reducers/drinksReducer.js
+++ src/reducers/drinksReducer.js
@@ -1,6 +1,6 @@
 let id = 0
 
-export default (state = [{name: "Manhattan", main_liquor: "Whiskey", ingredients: "2 oz rye whiskey, .75 oz sweet red vermouth, 1-2 dashes Angostura bitters, Maraschino cherry", instructions: "In a mixing glass, add rye, vermouth, bitters, and a large cube of ice. Stir. Strain into a chilled cocktail glass and garnish with a cherry."}], action) => {
+export default (state = [], action) => {
     const newDrink = () => {
         return {
             ...action.payload,
@@ -16,3 +16,5 @@ export default (state = [{name: "Manhattan", main_liquor: "Whiskey", ingredients
             return state
     }
 }
+
+// {name: "Manhattan", main_liquor: "Whiskey", ingredients: "2 oz rye whiskey, .75 oz sweet red vermouth, 1-2 dashes Angostura bitters, Maraschino cherry", instructions: "In a mixing glass, add rye, vermouth, bitters, and a large cube of ice. Stir. Strain into a chilled cocktail glass and garnish with a cherry."}
