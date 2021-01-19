6e4c72b9bca6a8ed6e1005911d1ce44db375b8bc
maxjacobzander <maxjacobzander@gmail.com>
2021-01-19 15:53:31 -0500

    removed extraneous browserrouter names in navbar.js

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import { NavLink } from 'react-router-dom';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 export class NavBar extends Component {
   render() {
