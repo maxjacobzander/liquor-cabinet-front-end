ad4cb945042337b4146c042aa1c5fb0d2b8c560c
maxjacobzander <maxjacobzander@gmail.com>
2021-01-22 10:17:29 -0500

    Added search functionality to all Drinks list

--- src/containers/Drinks.js
+++ src/containers/Drinks.js
@@ -5,18 +5,39 @@ import { fetchDrinks } from '../actions/actions';
 import LogoHeader from '../components/LogoHeader'
 
 export class Drinks extends Component {
+    constructor() {
+        super();
+        this.state = {
+            search: ''
+        }
+    }
+
+    updateSearch(event){
+        this.setState({search: event.target.value})
+    }
+
     componentDidMount(){
         this.props.fetchDrinks()
     }
 
     render() {
-        const drinks = this.props.drinks.map(( drink, i ) => <DrinkItem key={i} drink={drink} />)
+        // const drinks = this.props.drinks.map(( drink, i ) => <DrinkItem key={i} drink={drink} />)
+        let drinks = this.props.drinks.filter(
+            (drink) => {
+                return drink.main_liquor.indexOf(this.state.search) !== -1;
+            }
+        );
+        let filteredDrinks = drinks.map(( drink, i ) => <DrinkItem key={i} drink={drink} />)
         return (
             <div>
                 <LogoHeader />
                 <h2>Drinks</h2>
+                <input type="text" id="allDrinkSearchBar"
+                    placeholder="Please Enter A Type Of Liquor (ie. Whiskey, Tequila, etc.)"
+                    value={this.state.search}
+                    onChange={this.updateSearch.bind(this)} />
                 <ul classname="DrinkCollection">
-                    { drinks }
+                    { filteredDrinks }
                 </ul>
             </div>
         );
