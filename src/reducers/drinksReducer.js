6fd8e086bc399549c89e795a36fa0ff6e115a3bb
maxjacobzander <maxjacobzander@gmail.com>
2021-07-19 12:07:43 -0400

    Assigned reducer arrow function to a variable before exporting as default to remove warning

--- src/reducers/drinksReducer.js
+++ src/reducers/drinksReducer.js
@@ -1,4 +1,4 @@
-export default (state = [], action) => {
+const reducer = (state = [], action) => {
     switch(action.type){
         case "SET_DRINK":
             return [...action.payload]
@@ -10,3 +10,5 @@ export default (state = [], action) => {
             return state
     }
 }
+
+export default reducer
